f673588d280cdd599c526a16aefb441095edf8ba
erichiroshi <erichiroshi@gmail.com>
2020-09-29 23:16:01 -0400

    Aula 8 - Mostrando lista de produtos cadastrados anteriormente

--- src/br/com/programacaodesktop/dao/impl/ProdutoDaoJDBC.java
+++ src/br/com/programacaodesktop/dao/impl/ProdutoDaoJDBC.java
@@ -2,6 +2,10 @@ package br.com.programacaodesktop.dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.DefaultListModel;
 
 import br.com.programacaodesktop.dao.ProdutoDao;
 import br.com.programacaodesktop.db.DB;
@@ -24,16 +28,33 @@ public class ProdutoDaoJDBC implements ProdutoDao {
 								+ "(nome, preco) "
 								+ "VALUES "
 								+ "(?, ?)");
+			
 			st.setString(1, obj.getNome());
 			st.setDouble(2, obj.getPreco());
-			
+
 			st.execute();
 			System.out.println("Salvo!");
 		} catch (Exception e) {
 			throw new DbException(e.getMessage());
 		} finally {
 			DB.closeStatement(st);
-			DB.closeConnection();
+		}
+	}
+
+	@Override
+	public DefaultListModel<Produto> list() {
+		DefaultListModel<Produto> model = new DefaultListModel<>();
+
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM produto ");
+			ResultSet rs = st.executeQuery();
+			while (rs.next()) {
+				model.addElement(new Produto(rs.getString("nome"), rs.getDouble("preco")));
+			}
+			return model;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
 		}
 	}
 
